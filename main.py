89bab2db71d47c7c59b2c1e87b0c071b36633116
hcamp654 <hcamp654@bjalearn.net>
2026-02-18 10:25:52 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -11,33 +11,34 @@ def calculate_total_price(products):
 
 
 def apply_discount(price, discount):
+    price = float(price)
     if discount > 1:
-        discount = discount / 100
+        discount = float(discount) / 100
     return price - price * discount
 
 
 def update_stock(products, product_name, amount):
     for product in products:
         if product["name"] == product_name:
-            product["stock"] = product["stock"] - amount
+            product["stock"] = product["stock"] - int(amount)
     return product
 
 
 def get_average_price(products):
     total = calculate_total_price(products)
-    return total / products
+    return total / len(products)
 
 
 def print_products(products):
     for i in range(len(products)):
-        print(products["name"], "-", products[i]["price"], "-", products[i]["stock"])
+        print(products[i]["name"], "-", products[i]["price"], "-", products[i]["stock"])
 
 
 products = [
     {"name": "Pencil", "price": 0.99, "stock": 100},
-    {"name": "Notebook", "price": "2.50", "stock": 50},
-    {"name": "Backpack", "price": 25, "stock": "20"},
-    {"name": "Marker", "price": 1.5}
+    {"name": "Notebook", "price": 2.50, "stock": 50},
+    {"name": "Backpack", "price": 25.0, "stock": 20},
+    {"name": "Marker", "price": 1.5, "stock": 30},
 ]
 
 
@@ -59,22 +60,23 @@ average = get_average_price(products)
 print("Average price:", average)
 
 
-choice = input("Enter product name to buy: ")
-quantity = input("Enter quantity: ")
+choice = "Pencil"
+quantity = 1
 
 for product in products:
     if product["name"] == choice:
         if quantity <= product["stock"]:
             product["stock"] = product["stock"] - quantity
             print("Purchase successful")
-        else
+    else:
             print("Not enough stock")
 
 
 count = 0
 while count < len(products):
     print(products[count]["name"])
-    count = count - 1
+    count = count + 1
+
 
 
 print("Done")
